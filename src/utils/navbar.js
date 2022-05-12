43ab3e06ef77dc9e2c478e3a08b09ad93be774fc
vaha77 <abdulvohid996@gmail.com>
2022-05-13 00:59:53 +0900

    ls

---
+++ src/utils/navbar.js
@@ -0,0 +1,26 @@
+export const navber = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/home",
+    search: "?",
+    hidden: false,
+    private: false,
+  },
+  {
+    id: 2,
+    title: "Properties",
+    path: "/properties",
+    search: "?",
+    hidden: false,
+    private: false,
+  },
+  {
+    id: 1,
+    title: "Home",
+    path: "/home",
+    search: "?",
+    hidden: false,
+    private: false,
+  },
+];
