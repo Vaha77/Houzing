29ced0c44f4af0eeb4617108636266da9936426b
vaha77 <abdulvohid996@gmail.com>
2022-05-17 00:23:34 +0900

    ls

--- src/components/Home/Carusel/index.jsx
+++ src/components/Home/Carusel/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Container, Img } from "./style";
+import { Container, Icon, Img } from "./style";
 import img1 from "../../../asset/imgs/img1.png";
 import img2 from "../../../asset/imgs/img2.png";
 export const Carusel = () => {
   return (
-    <Container dots autoplay>
-      <Img src={img1} />
-      <Img src={img2} />
-    </Container>
+    <>
+      <Icon.Left />
+      <Icon.Right />
+      <Container dots autoplay>
+        <Img src={img1} />
+        <Img src={img2} /> <Img src={img1} />
+        <Img src={img2} /> <Img src={img1} />
+        <Img src={img2} />
+      </Container>
+    </>
   );
 };
 export default Carusel;
--- src/components/Home/Carusel/style.js
+++ src/components/Home/Carusel/style.js
@@ -1,13 +1,61 @@
 import { Carousel } from "antd";
 import styled from "styled-components";
-
+import { ReactComponent as arrow } from "../../../asset/icons/arrow.svg";
 const Container = styled(Carousel)`
   width: 100%;
-  height: 570px;
 `;
 const Img = styled.img`
   background: black;
   width: 100%;
   height: 570px;
 `;
-export { Container, Img };
+const Icon = styled.div``;
+Icon.Left = styled(arrow)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 0px;
+  transform: translate(100%, -50%);
+  width: 45px;
+  height: 45px;
+  color: white;
+  background: white;
+  padding: 12px;
+  opacity: 0.5;
+  border-radius: 50%;
+  z-index: 999;
+  cursor: pointer;
+  :hover {
+    opacity: 0.97;
+  }
+  & path {
+    fill: #0d263b;
+  }
+`;
+Icon.Right = styled(arrow)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  right: 0px;
+  transform: translate(100%, -40%) rotate(180deg);
+  width: 45px;
+  height: 45px;
+  color: white;
+  background: white;
+  padding: 12px;
+  opacity: 0.5;
+  border-radius: 50%;
+  z-index: 999;
+  cursor: pointer;
+  :hover {
+    opacity: 0.97;
+  }
+  & path {
+    fill: #0d263b;
+  }
+`;
+export { Container, Img, Icon };
