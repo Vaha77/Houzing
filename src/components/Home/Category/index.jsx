a85f7b6bf3f1120f804df1e014dcffd76f153888
vaha77 <abdulvohid996@gmail.com>
2022-05-27 16:56:48 +0900

    sc

--- src/components/Home/Category/index.jsx
+++ src/components/Home/Category/index.jsx
@@ -1,19 +1,42 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import AliceCarousel from "react-alice-carousel";
-import Card from "../../Card";
+import { useQuery } from "react-query";
 import { Container, Cards, ArrowLeft, Wrapper, ArrowRight } from "./style";
+const { REACT_APP_BASE_URL: url } = process.env;
 
-export const Category = () => {
-  const items = [
-    <Card mr={20} />,
-    <Card mr={20} />,
-    <Card mr={20} />,
-    <Card mr={20} />,
-    <Card mr={20} />,
-  ];
+const Title = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+const Category = () => {
+  const [list, setList] = useState([]);
+
   const slider = useRef();
+
+  useQuery(
+    "",
+    () => {
+      return fetch(`${url}/v1/categories`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }).then((res) => res.json());
+    },
+    {
+      onSuccess: (res) => {
+        console.log(res, "res");
+        let response = res?.List?.[0]?.map((value) => (
+          <div style={{ height: "200px", width: "200px" }}>
+            <Title title={value} />
+          </div>
+        ));
+        setList(response || []);
+      },
+    }
+  );
+
   return (
-    <Container className="nocopy">
+    <Container>
       <div className="title center">Category</div>
       <div className="description center">
         Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
@@ -25,7 +48,7 @@ export const Category = () => {
             ref={slider}
             autoWidth
             mouseTracking
-            items={items}
+            items={list}
           />
           <ArrowRight onClick={() => slider.current?.slidePrev()}>
             &lang;
